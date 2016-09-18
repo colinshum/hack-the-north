074cc3bb4ead0ae308d3e013f1d28336b5310b0b
Colin Shum <colinshum@v1020-wn-44-50.campus-dynamic.uwaterloo.ca>
2016-09-18 00:12:24 -0400

    Timer Integration
    s/o to @wolfishflow

--- productiivity/app/src/main/java/com/cshum/productiivity/MainActivity.java
+++ productiivity/app/src/main/java/com/cshum/productiivity/MainActivity.java
@@ -1,13 +1,58 @@
 package com.cshum.productiivity;
 
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public TextView label;
+
+    public double timerValue;
+    public MediaPlayer media;
+    public int git = R.raw.git;
+    public int rah = R.raw.rah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gitStart();
+    }
+
+    public void gitStart() {
+        media = MediaPlayer.create(this, git);
+        media.start();
+    }
+
+    public void rahStart() {
+        media = MediaPlayer.create(this, rah);
+        media.start();
+    }
+
+    public void timerStart() {
+
+    }
+
+    public void startTimer(View view) {
+        label = (TextView) findViewById(R.id.labelTxt);
+        EditText time = (EditText) findViewById(R.id.editTxt);
+
+        label.setText(time.getText().toString());
+        timerValue = Double.parseDouble(time.getText().toString());
+
+        new CountDownTimer(Long.parseLong(time.getText().toString())*60000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                label.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+            public void onFinish() {
+                label.setText("Finished!");
+                rahStart();
+            }
+        }.start();
     }
 }
+
