0b2c6253ca768768756e7a7c05461ecdec4d701b
Colin Shum <colinshum@v1020-wn-44-50.campus-dynamic.uwaterloo.ca>
2016-09-18 01:22:14 -0400

    what ???

--- productiivity/app/src/main/java/com/cshum/productiivity/MainActivity.java
+++ productiivity/app/src/main/java/com/cshum/productiivity/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.os.Vibrator;
+
 public class MainActivity extends AppCompatActivity {
     public TextView label;
 
